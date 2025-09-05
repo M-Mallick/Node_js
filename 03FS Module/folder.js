22cd82a0822cd429a131021e4afdb90817f740f7
Mridu Mallick <mridumallick101@gmail.com>
2025-09-05 12:22:10 +0530

    Update folder.js

--- 03FS Module/folder.js
+++ 03FS Module/folder.js
@@ -1,1 +1,7 @@
-aa
+// create a new folder
+const fs = require('fs');
+fs.mkdir("G:\\WEAB_DEVELOPMENT\\Backend With JS\\Node js\\Path Module\\CCC\\BBB\\NNN", { recursive: true }, (err) => {
+    if(err) {
+        console.log("Something went wrong");
+    }
+})
