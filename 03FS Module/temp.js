dc95ba02d51aaaa7b8f6409c0a424c62261c4796
Mridu Mallick <mridumallick101@gmail.com>
2025-09-05 12:19:05 +0530

    Update temp.js

--- 03FS Module/temp.js
+++ 03FS Module/temp.js
@@ -1,1 +1,23 @@
-cyfjhv
+const fs = require('fs');
+// Asyncronus Method
+console.log("Start");
+fs.readFile('text1.txt','utf8', (err, data) => {
+    if(err) {
+        console.log("error: "+err);
+    }else{
+        console.log(data);
+    }
+})
+console.log("End");
+
+// Synchronus Mryhod
+console.log("Start");
+
+try{
+    const file = fs.readFileSync('text1.txt', 'utf8');
+    console.log("File content: ", file);
+}
+catch(err) {
+    console.log("Error: ", err);
+}
+console.log("end");
