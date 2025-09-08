8173a0300ace7fb2923a1bb7edb255e9d91b092d
Mridu Mallick <mridumallick101@gmail.com>
2025-09-08 23:23:08 +0530

    Create temp.js

---
+++ 05HTTP Module/Concept/temp.js
@@ -0,0 +1,15 @@
+const http = require('node:http');
+const PORT = 8000;
+
+const sever = http.createServer((req, res) => {
+    console.log(req.method);
+    
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify({name:"Mridu"}));
+    res.end(JSON.stringify({name:"Adima"}));
+})
+
+sever.listen(PORT,() => {
+    console.log("Server is running...");
+    
+});
